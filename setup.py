4219bcfa346cd45bc37813b9708c4e035db48984
Lakshay Arora <lakshayaroraq@gmail.com>
2025-07-09 13:50:55 +0530

    All requirement and read me updated

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Lakshay",
+    author_email="lakshayaroaq@gmail.com",
+    packages=find_packages()
+)
